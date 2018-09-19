1286695e31a1a70b9ef4e5029d5bb6fb7c97136d
Ajith <jajith004@gmail.com>
2018-09-19 14:23:22 +0530

    Update api.js

--- api.js
+++ api.js
@@ -6,3 +6,14 @@
 // })
 
 alert("thanks123");
+
+    var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(xhttp.responseText);
+      // Typical action to be performed when the document is ready:
+      //    document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "http://demo8830156.mockable.io/getsample", true);
+  xhttp.send();
